f4246fa33596a968c65ce2c5b7e3e7a5bb9b0896
Walter Karas <wkaras@oath.com>
2018-10-31 14:46:44 -0500

    Cause traffic_server to die fast if a plugin is loaded that was not built with the correct core version.
    And with same compiler version as core.

--- build/plugins.mk
+++ build/plugins.mk
@@ -16,13 +16,21 @@
 
 # plugins.mk: Common automake build variables for Traffic Server plugins.
 
+GCC_VER_GET = $(CC) --version | tr ' ' '\\n' | grep --color=auto -F . | head -1
+CLANG_VER_GET = $(CC) -dumpversion
+# Assume clang if not gcc
+COMP_NAME := $(shell bash -c "if $(CC) --version | grep --color=auto -F gcc ; then echo gcc ; else echo clang ; fi")
+COMP_FULL_VER := $(shell bash -c "if $(CC) --version | grep --color=auto -F gcc ; then $(GCC_VER_GET) ; else $(CLANG_VER_GET) ; fi")
+COMP_MAJOR_VER := $(shell bash -c "echo $(COMP_FULL_VER) | tr '.' '\n' | head -1")
+COMP_MINOR_VER := $(shell bash -c "echo $(COMP_FULL_VER) | tr '.' '\n' | head -2 | tail -1")
+
 # Default plugin LDFLAGS. We don't use TS_PLUGIN_LDFLAGS because
 # that is an automake canonical variable name.
 TS_PLUGIN_LD_FLAGS = \
   -module \
   -shared \
   -avoid-version \
-  -export-symbols-regex '^(TSRemapInit|TSRemapDone|TSRemapDoRemap|TSRemapNewInstance|TSRemapDeleteInstance|TSRemapOSResponse|TSPluginInit)$$'
+  -export-symbols-regex '^(TSRemapInit|TSRemapDone|TSRemapDoRemap|TSRemapNewInstance|TSRemapDeleteInstance|TSRemapOSResponse|TSPluginInit_@TS_VERSION_MAJOR@)_$(COMP_NAME)_$(COMP_MAJOR_VER)_$(COMP_MINOR_VER)$$'
 
 TS_PLUGIN_CPPFLAGS = \
   -I$(abs_top_builddir)/proxy/api \
